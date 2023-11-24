4962f3678d9c73dc19ac32a2d24283ed438cfddf
sergpapa <>
2023-11-24 12:48:52 +0000

    added javascript button card hover styles and button hover styles

---
+++ assets/scripts/main.js
@@ -0,0 +1,13 @@
+const card = document.getElementsByClassName("card");
+
+for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener("mouseover", function () {
+        let button = document.getElementById(`button-${i+1}`);
+        button.classList.add("bg-gold");
+    });
+    card[i].addEventListener("mouseout", function () {
+        let button = document.getElementById(`button-${i + 1}`);
+        button.classList.remove("bg-gold");
+    });
+    
+}
